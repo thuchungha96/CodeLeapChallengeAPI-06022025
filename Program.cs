3879c361502876f987fb604d6472ba6235602a35
ha.thuchung96 <ha.thuchung96@gmail.com>
2025-02-07 10:29:28 +0700

    [Modify] Allow open swagger with all enviroment

--- Program.cs
+++ Program.cs
@@ -79,7 +79,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
